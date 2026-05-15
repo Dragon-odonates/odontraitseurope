99760fc889e1e0e38870e2430dc96d8623555dec
Thore Engel <thore.engel@idiv.de>
2026-05-15 13:44:23 +0200

    Update data for revision

--- analyses/01_make_csvs.R
+++ analyses/01_make_csvs.R
@@ -1,9 +1,9 @@
 # Load all packages listed in DESCRIPTION
 devtools::install_deps(upgrade = "never")
 devtools::load_all()
-
+library(tidyverse)
 # Path to the Excel file
-file_path <- here::here("data", "20251212-OdonTraits_Europe.xlsx")
+file_path <- here::here("data", "OdonTraits_Europe_vs2.xlsx")
 
 # Specify output folder
 output_dir <- here::here("data", "csv-files/revision/")
@@ -16,7 +16,20 @@ for (s in sheets) {
   
   # Read sheet
   df <- readxl::read_excel(file_path, sheet = s)
-  #df <- dplyr::select(df, -ends_with("_source"))
+  
+  # Drop columns that are entirely NA
+  df <- df[, colSums(!is.na(df)) > 0, drop = FALSE]
+  
+  # In the 'sources' sheet, strip "_source" suffix so all column names are uniform
+  if (s == "sources") {
+    names(df) <- sub("_source$", "", names(df))
+  }
+  
+  #
+  if (s == "references") {
+    names(df) <- c("short_reference", "long_reference")
+    df <- df |> filter(!is.na(long_reference))
+  }
   
   # Clean name for use both as object name and as filename
   clean_name <- gsub("[^A-Za-z0-9_]", "_", s)
@@ -27,6 +40,8 @@ for (s in sheets) {
   # Create CSV filename
   csv_name <- paste0(clean_name, ".csv")
   output_path <- file.path(output_dir, csv_name)
+  
+
   
   # Write with UTF-8 encoding, comma separator, dot decimal
   write.table(
@@ -41,3 +56,18 @@ for (s in sheets) {
   
   message("Loaded + Exported: ", clean_name, " → ", output_path)
 }
+
+#export names of file "sources"
+
+sources_description = data.frame(
+  Column_label = names(sources)
+  )
+
+sources_description <-
+  sources_description |> 
+  mutate(column_description= paste0("Data source for ", Column_label, " as short reference"))
+
+write_excel_csv(sources_description, "data/sources_description.csv")
+
+
+
